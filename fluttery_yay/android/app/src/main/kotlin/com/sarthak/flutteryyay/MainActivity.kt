8cf1147d1c3d19ec4ce7a30ae89bbcd05a34a6cd
CodeSarthak <getsarthakaggarwal@gmail.com>
2020-06-04 19:50:33 +0530

    init

---
+++ fluttery_yay/android/app/src/main/kotlin/com/sarthak/flutteryyay/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sarthak.flutteryyay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fluttery_yay/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Calling, by Sarthak',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatefulWidget {
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
+
+
+
+
